8dab2040ff6b3b35f39d83fac260dc9647b4ebf8
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-05 16:55:15 -0700

    Created four different components.

--- tutorial/freeCodeCamp/starting-app/src/App.js
+++ tutorial/freeCodeCamp/starting-app/src/App.js
@@ -1,17 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
-        <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-
-        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-      </header>
-
-    </div>
+    <div>Hello World!</div>
   );
 }
 
