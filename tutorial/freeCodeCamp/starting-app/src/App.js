0ad6b5a82b0a66ca4c06cdedd55f24bc5ee3f33d
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-05 15:29:52 -0700

    Adding more modified files in this app.

---
+++ tutorial/freeCodeCamp/starting-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
+        <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+
+        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
