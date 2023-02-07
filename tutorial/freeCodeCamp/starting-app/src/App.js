24dd0e9d7ae603d7e4f5d0bec7e76aec10055174
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-07 08:37:16 -0700

    Rendering list in React
    Created a list in JSON format.

--- tutorial/freeCodeCamp/starting-app/src/App.js
+++ tutorial/freeCodeCamp/starting-app/src/App.js
@@ -5,8 +5,27 @@ import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 
 function App() {
+
+  const contacts = [
+    {
+      id: "1",
+      name: "Dave",
+      email: 'dave@email.com'
+    },
+    {
+      id: "2",
+      name: "Mary",
+      email: 'mary@email.com'
+    },
+    {
+      id: "3",
+      name: "Riley",
+      email: 'riley@email.com'
+    }
+  ];
+
   return (
-    <div>
+    <div className='ui container'>
       <Header/>
       <AddContact/>
       <ContactList/>
