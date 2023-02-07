4e25598843c2755aa00e03d320ab7cd9d0f745fe
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-07 12:30:08 -0700

    useEffect Hook used for retrieving and deleting data

--- tutorial/freeCodeCamp/starting-app/src/App.js
+++ tutorial/freeCodeCamp/starting-app/src/App.js
@@ -1,25 +1,53 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
+import { uuid } from 'uuidv4'; 
 
 function App() {
+
+  const LOCAL_STORAGE_KEY = "contacts";
 
   const [contacts, setContacts] = useState([]);
 
   const addingContactHandler = (contact) => {
     console.log(contact);
 
-    setContacts([...contacts, contact])
-    //2nd parameter is to add the new contacts.
+    setContacts([...contacts, { id: uuid(), ...contact }])
+    //1st parameter is to set a REST operator of the vars from AddContact
+    //2nd parameter is adding a unique id for each new contact.
   }
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+      //This var will have a list of contacts not matching the id.
+    });
+
+    setContacts(newContactList);
+  }
+
+  //Storing values in the local storage after submission.
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  //This hook will retrieve contacts already stored and display it on the page,
+  //even when the page is reloaded.
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    
+    if(retrieveContacts) {
+      setContacts(retrieveContacts);
+    }
+  }, []);
 
   return (
     <div className='ui container'>
       <Header/>
       <AddContact addContactHandler={ addingContactHandler }/>
-      <ContactList contacts={ contacts }/>
+      <ContactList contacts={ contacts } getContactID={removeContactHandler}/>
     </div>
   );
 }
--- tutorial/freeCodeCamp/starting-app/src/components/ContactList.js
+++ tutorial/freeCodeCamp/starting-app/src/components/ContactList.js
@@ -5,9 +5,17 @@ const ContactList = (props) => {
     console.log(props); 
     //To show the contacts (props) was accessed from App to this file.
 
+    const deleteContactHandler = (id) => {
+        props.getContactID(id);
+    };
+
     const renderContactList = props.contacts.map((contact) => {
         return(
-            <ContactCard propVariable={contact}></ContactCard>
+            <ContactCard 
+                propVariable={contact} 
+                clickHandler={ deleteContactHandler } 
+                key={contact.id}
+            />
             //Passing props from parent to child component
         );
     });
