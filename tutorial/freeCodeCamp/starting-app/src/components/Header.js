c6a13a5c5c7687ff919fbe10077337b20204569a
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-07 08:33:30 -0700

    Added features to the four components.

--- tutorial/freeCodeCamp/starting-app/src/components/Header.js
+++ tutorial/freeCodeCamp/starting-app/src/components/Header.js
@@ -1,2 +1,16 @@
 /*Link: https://www.youtube.com/watch?v=0riHps91AzE
 Stopped at  */
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='ui fixed menu'>
+            <div className='ui container center'>
+                <h2>Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
