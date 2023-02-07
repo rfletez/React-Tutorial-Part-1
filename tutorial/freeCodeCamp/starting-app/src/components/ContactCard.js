e04a6414d16cc74cc990371eb77af8da6a6103cc
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-07 09:01:38 -0700

    Passing props again
    Passed props from ContactList to ContactCard, and inline style to access variables.

--- tutorial/freeCodeCamp/starting-app/src/components/ContactCard.js
+++ tutorial/freeCodeCamp/starting-app/src/components/ContactCard.js
@@ -1,1 +1,20 @@
 import React from 'react';
+
+const ContactCard = (props) => {
+    const {id, name, email} = props.propVariable;
+    //Faster alternative to props.contacts.map((contact), and getting the variables.
+
+    return(
+        <div className='item'>
+            <div className='content'>
+                <div className='header'> {name} </div>
+                <div>{email}</div>
+            </div>
+            <i className='trash alternate outline icon' 
+                style={{color:'red', marginTop:'7px'}}>
+            </i>
+        </div>
+    );
+}
+
+export default ContactCard;
--- tutorial/freeCodeCamp/starting-app/src/components/ContactList.js
+++ tutorial/freeCodeCamp/starting-app/src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactCard from './ContactCard';
 
 const ContactList = (props) => {
     console.log(props); 
@@ -6,13 +7,8 @@ const ContactList = (props) => {
 
     const renderContactList = props.contacts.map((contact) => {
         return(
-            <div className='item'>
-                <div className='content'>
-                    <div className='header'> {contact.name} </div>
-                    <div>{contact.email}</div>
-                </div>
-                <i className='trash alternate outline icon'></i>
-            </div>
+            <ContactCard propVariable={contact}></ContactCard>
+            //Passing props from parent to child component
         );
     });
     return (
