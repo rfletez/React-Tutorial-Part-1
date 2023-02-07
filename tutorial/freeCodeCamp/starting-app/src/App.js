fe0033aeccfb085df33bc8d86d9066b784be0a46
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-02-07 08:19:51 -0700

    Modified App.js with other components.

--- tutorial/freeCodeCamp/starting-app/src/App.js
+++ tutorial/freeCodeCamp/starting-app/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import AddContact from './components/AddContact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <Header/>
+      <AddContact/>
+      <ContactList/>
+    </div>
   );
 }
 
