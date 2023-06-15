8ba9b19ed3cdd1bd2148aa38dcde631f8b867179
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-15 14:40:06 -0700

    Function component
    Created Header as a functional component.

--- tutorial/Malvia/contact-app/src/components/App.js
+++ tutorial/Malvia/contact-app/src/components/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      Hello World!
+      <Header/>
+      <AddContact/>
+      <ContactList/>
+    </div>
   );
 }
 
