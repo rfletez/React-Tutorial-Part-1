74b25894d842af374a7f893bf81a73544c8dc9e2
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-16 11:59:44 -0700

    Passing props from Child to Parent

--- tutorial/Malvia/contact-app/src/components/App.js
+++ tutorial/Malvia/contact-app/src/components/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
 
-  const contacts = [
-    {
-      id: 1,
-      name: "ross",
-      email: "ross@email.com"
-    },
-    {
-      id: 2,
-      name: "mike",
-      email: "mike@email.com"
-    }
-  ];
+  const [contacts, useContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(`From App Component: ${contact}`);
+  }
 
   return (
     <div className='ui container'>
       Hello World!
       <Header/>
-      <AddContact/>
+      <AddContact addContactHandlerVariable={addContactHandler}/>
       <ContactList contacts={ contacts } />
     </div>
   );
--- tutorial/Malvia/contact-app/src/components/ContactCard.js
+++ tutorial/Malvia/contact-app/src/components/ContactCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import userImg from '../images/user.jpeg';
 
 const ContactCard = (props) => {
     const [id, name, email] = props.contact;
 
     return(
         <div className='item'>
+            <img className='ui avatar image' src={userImg} alt='user' />
             <div className='content'>
                 <div className='header'>{name}</div>
                 <div>{email}</div>
