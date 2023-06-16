715a22d42b190a84209094cae3f0412b3849d302
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-06-16 09:25:52 -0700

    Access Props from Parent to Child

--- tutorial/Malvia/contact-app/src/components/App.js
+++ tutorial/Malvia/contact-app/src/components/App.js
@@ -5,12 +5,26 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
+
+  const contacts = [
+    {
+      id: 1,
+      name: "ross",
+      email: "ross@email.com"
+    },
+    {
+      id: 2,
+      name: "mike",
+      email: "mike@email.com"
+    }
+  ];
+
   return (
     <div className='ui container'>
       Hello World!
       <Header/>
       <AddContact/>
-      <ContactList/>
+      <ContactList contacts={ contacts } />
     </div>
   );
 }
