d2d5c6a2daf6359c1230fbe91d8571b580ea14c7
Rosny Fletez Jr <rfletezjr@gmail.com>
2023-07-21 15:31:07 -0700

    Created useEffect for Life Cycle Hooks

--- tutorial/Malvia/React/myfirstapp/src/components/Clock.js
+++ tutorial/Malvia/React/myfirstapp/src/components/Clock.js
@@ -2,13 +2,30 @@ import React, {useState, useEffect} from 'react';
 
 function Clock() {
     const [time, setTime] = useState(new Date().toString());
+    const [flag, setFlag] = useState(true);
+
+    useEffect(() => {
+        console.log("Clock Component is mounted or updated.");
+    }, [flag]); 
+    //useEffect will run when the dependency array with state variable (flag) 
+    //changes its value.
+
+    useEffect(() => {
+        const interval = setInterval(showDate(), 5000);
+        console.log("Time variable is updated.");
+
+        return () => {
+            console.log("Cleanup of interval.");
+            clearInterval(interval);
+        };
+
+    }, [time]);
+
 
     function showDate() {
         console.log(new Date().toString());
         setTime(new Date().toString());
     }
-
-    setInterval(showDate(), 5000);
 
     return( <div>{time}</div>) ;
 }
